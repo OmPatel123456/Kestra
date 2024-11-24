7ed5ddcee854b50a8819607b5f4e3d66b4c8cf2d
RyanVem <ryanvemulapalli@gmail.com>
2024-11-23 23:09:28 -0500

    Fixed #5499, added cancel button

--- ui/src/components/executions/Resume.vue
+++ ui/src/components/executions/Resume.vue
@@ -17,6 +17,9 @@
             <inputs-form :initial-inputs="inputsList" :execution="execution" v-model="inputs" />
         </el-form>
         <template #footer>
+            <el-button @click="cancel()">
+                {{ $t('cancel') }}
+            </el-button>
             <el-button :icon="PlayBox" type="primary" @click="resumeWithInputs($refs.form)" native-type="submit">
                 {{ $t('resume') }}
             </el-button>
@@ -103,6 +106,16 @@
                     namespace: this.execution.namespace
                 });
             },
+            cancel() {
+                this.$store
+                    .dispatch("execution/kill", {
+                        id: this.execution.id,
+                    })
+                    .then(() => {
+                        this.isDrawerOpen = false;
+                        this.$toast().success(this.$t("cancel done"));
+                    });
+            }, 
         },
         computed: {
             ...mapState("auth", ["user"]),
